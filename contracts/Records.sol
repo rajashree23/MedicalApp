f28b56ff64184a85cce684076d147a2649d1f885
Rajashree Parhi <rajashreeparhi23@gmail.com>
2019-12-11 19:31:44 +0530

    Client dir visible

--- contracts/Records.sol
+++ contracts/Records.sol
@@ -53,7 +53,6 @@ function loginDoctor(address doctor) public{
 
 //give permission to doctor to access patient's address
 function createPermission(address patient) public{
-  
   doctors[msg.sender].pat[patient] = true;
   emit docPat(patient,true);
 }
@@ -63,6 +62,7 @@ function patientRecord(address patient, string memory hash)public{
   patients[patient].patRecords.push(hash);
 }
 
+//verify patient
 function verify(address patient) public returns(bool){
   return doctors[msg.sender].pat[patient];
 }
